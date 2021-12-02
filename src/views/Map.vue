f8c22edf0b8dca608cb08d0e1cc507d2fb906632
Ryan Laird <lairdry@gmail.com>
2021-12-02 17:37:06 -0600

    updated basemap to custom mapbox style

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -294,7 +294,7 @@ export default {
       fillColor2: "#91251dFF",
       chooseColor: false,
       chooseColor2: false,
-      url: 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png',
+      url: 'https://api.mapbox.com/styles/v1/rlaird2/ckwo8jew23mns14lrzvnnxh63/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoicmxhaXJkMiIsImEiOiJja2JmN2x6aWIwc3VmMzVvNDl5Mzk1ejNuIn0.rrNaMaCy39_ntp7qPvp0dQ',
       attribution:
       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions>CARTO</a>',
     };
@@ -416,7 +416,7 @@ export default {
   methods: {
     recenterMap() {
       this.$refs.myMap.mapObject.flyTo([39, -109], 6)
-  }
+    }
   }
 };
 
